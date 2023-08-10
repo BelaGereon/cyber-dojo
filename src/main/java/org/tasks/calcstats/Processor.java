de0d5439d3ba04b3498edf12bac5d61763a1827a
Bela Géréon <belagereon@gmx.de>
2023-08-10 13:20:01 +0200

    implement getMaxValue functionality

--- src/main/java/org/tasks/calcstats/Processor.java
+++ src/main/java/org/tasks/calcstats/Processor.java
@@ -3,11 +3,15 @@ package org.tasks.calcstats;
 public class Processor {
     public int getMinValue(int[] integers) {
         int minValue = integers[0];
-        for (int integer : integers) {
-            if (integer < minValue) {
-                minValue = integer;
-            }
-        }
+        for (int integer : integers)
+            if (integer < minValue) minValue = integer;
         return minValue;
+    }
+
+    public int getMaxValue(int[] integers) {
+        int maxValue = integers[0];
+        for (int integer : integers)
+            if (integer > maxValue) maxValue = integer;
+        return maxValue;
     }
 }
--- src/test/java/org/tasks/calcstats/ProcessorTest.java
+++ src/test/java/org/tasks/calcstats/ProcessorTest.java
@@ -8,22 +8,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ProcessorTest {
 
     private Processor processor;
+    int[] integers1;
+    int[] integers2;
 
     @BeforeEach
     void setup() {
         processor = new Processor();
+        integers1 = new int[]{6, 9, 15, -2, 92, 11};
+        integers2 = new int[]{1, 2, 3, 4, 5, 6};
+
     }
     @Test
-    void shouldBeAbleToDetermineMinimumValue() {
-        // given
-        int[] integers1 = {3, 2, 1};
-        int[] integers2 = {10, 11, 12};
-
+    void shouldBeAbleToDetermineMinimumValue_whenDifferentInputsAreProvided() {
         int smallestInteger1 = processor.getMinValue(integers1);
         int smallestInteger2 = processor.getMinValue(integers2);
 
-        assertEquals(1, smallestInteger1);
-        assertEquals(10, smallestInteger2);
+        assertEquals(-2, smallestInteger1);
+        assertEquals(1, smallestInteger2);
+    }
+
+    @Test
+    void shouldBeAbleToDetermineMaximumValue_whenDifferentInputsAreProvided() {
+        int biggestInteger1 = processor.getMaxValue(integers1);
+        int biggestInteger2 = processor.getMaxValue(integers2);
+
+        assertEquals(92, biggestInteger1);
+        assertEquals(6, biggestInteger2);
     }
 
 }
