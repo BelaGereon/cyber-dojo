7bbd1cf7a6052dce8c1c0b2a4d020d940d7da5a0
Bela Géréon <belagereon@gmx.de>
2023-08-17 18:55:23 +0200

    add unit test for sort list in reversed order

--- src/test/java/org/tasks/sortingalgorithms/OrganizerTest.java
+++ src/test/java/org/tasks/sortingalgorithms/OrganizerTest.java
@@ -17,6 +17,7 @@ public class OrganizerTest {
     void setup() {
         list = new ArrayList<>();
     }
+
     @Test
     void shouldReturnNoElementsException_whenGivenListIsEmpty() {
         String expectedErrorMessage = "The list is empty and can not be sorted!";
@@ -34,21 +35,45 @@ public class OrganizerTest {
     }
 
     @Test
-    void shouldReturnSortedList_whenListHasMoreThanOneElement() throws EmptyListException {
+    void shouldSortListAscending() {
         final int[] testdataArray = new int[]{8, 98, 1, 45, 435, -2, 87, -143, Integer.MAX_VALUE};
         final int[] sortedTestdataArray = new int[]{-143, -2, 1, 8, 45, 87, 98, 435, Integer.MAX_VALUE};
 
         list = fillListWithArrayElements(testdataArray);
         List<Integer> expectedSortedList = fillListWithArrayElements(sortedTestdataArray);
 
-        final List<Integer> sortedList = organizer.sort(list);
+        final List<Integer> sortedList;
+
+        try {
+            sortedList = organizer.sort(list);
+        } catch (EmptyListException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertEquals(expectedSortedList, sortedList);
+    }
+
+    @Test
+    void shouldSortListDescending() {
+        final int[] testdataArray = new int[]{8, 98, 1, 45, 435, -2, 87, -143, Integer.MAX_VALUE};
+        list = fillListWithArrayElements(testdataArray);
+
+        final int[] sortedTestdataArray = new int[]{Integer.MAX_VALUE, 435, 98, 87, 45, 8, 1, -2, -143};
+        List<Integer> expectedSortedList = fillListWithArrayElements(sortedTestdataArray);
+
+        final List<Integer> sortedList;
+        try {
+            sortedList = organizer.sort(list);
+        } catch (EmptyListException e) {
+            throw new RuntimeException(e);
+        }
 
         assertEquals(expectedSortedList, sortedList);
     }
 
     private List<Integer> fillListWithArrayElements(int[] array) {
         List<Integer> filledList = new ArrayList<>();
-        for (int element: array) {
+        for (int element : array) {
             filledList.add(element);
         }
         return filledList;
