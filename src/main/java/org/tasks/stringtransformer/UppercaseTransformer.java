fa30e784c32781a0510a94134b602ff2d7cf4534
Bela Géréon <belagereon@gmx.de>
2023-11-06 09:15:51 +0100

    Make unit test pass

--- src/main/java/org/tasks/stringtransformer/UppercaseTransformer.java
+++ src/main/java/org/tasks/stringtransformer/UppercaseTransformer.java
@@ -2,7 +2,6 @@ package org.tasks.stringtransformer;
 
 public class UppercaseTransformer {
     public String transform(String string) {
-
-        return string;
+        return string.isEmpty() ? "" : string.toUpperCase();
     }
 }
