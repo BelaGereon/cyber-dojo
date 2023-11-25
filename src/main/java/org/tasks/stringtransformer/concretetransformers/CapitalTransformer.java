00ee1f7f5d3d215499a3034a904a2ae9a0dc5162
Bela Géréon <belagereon@gmx.de>
2023-11-25 19:53:04 +0100

    pass 3rd requirement

--- src/main/java/org/tasks/stringtransformer/concretetransformers/CapitalTransformer.java
+++ src/main/java/org/tasks/stringtransformer/concretetransformers/CapitalTransformer.java
@@ -2,9 +2,16 @@ package org.tasks.stringtransformer.concretetransformers;
 
 import org.tasks.stringtransformer.Transformer;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class CapitalTransformer implements Transformer {
     @Override
     public String sort(String string) {
-        return "";
+        return string.isEmpty() ? "" :
+                Arrays.stream(
+                string.split("\\s+"))
+                        .map(str -> str.substring(0, 1).toUpperCase() + str.substring(1))
+                        .collect(Collectors.joining(" "));
     }
 }
--- src/test/java/org/tasks/stringtransformer/CapitalTransformerTest.java
+++ src/test/java/org/tasks/stringtransformer/CapitalTransformerTest.java
@@ -13,9 +13,22 @@ public class CapitalTransformerTest {
         StringTransformer capitalTransformer = new StringTransformer();
         capitalTransformer.setTransformer(new CapitalTransformer());
 
+        // when then
         assertEquals("", capitalTransformer.transform(""));
     }
 
+    @Test
+    void shouldReturnString_withAllFirstLettersAsCapitalLetters() {
+        // given
+        StringTransformer capitalTransformer = new StringTransformer();
+        capitalTransformer.setTransformer(new CapitalTransformer());
+        String string = "every first letter of each word should be upper case.";
 
+        // when
+        String result = capitalTransformer.transform(string);
+
+        // then
+        assertEquals("Every First Letter Of Each Word Should Be Upper Case.", result);
+    }
 
 }
