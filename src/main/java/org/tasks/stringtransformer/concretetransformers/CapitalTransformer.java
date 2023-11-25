d6e9dd96a7be9f93699f95582a80ffde65167369
Bela Géréon <belagereon@gmx.de>
2023-11-25 19:59:27 +0100

    refactoring test solution to be more readable

--- src/main/java/org/tasks/stringtransformer/concretetransformers/CapitalTransformer.java
+++ src/main/java/org/tasks/stringtransformer/concretetransformers/CapitalTransformer.java
@@ -3,15 +3,20 @@ package org.tasks.stringtransformer.concretetransformers;
 import org.tasks.stringtransformer.Transformer;
 
 import java.util.Arrays;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CapitalTransformer implements Transformer {
     @Override
     public String sort(String string) {
+        String spaceRegex = "\\s+";
         return string.isEmpty() ? "" :
-                Arrays.stream(
-                string.split("\\s+"))
-                        .map(str -> str.substring(0, 1).toUpperCase() + str.substring(1))
+                Arrays.stream(string.split(spaceRegex))
+                        .map(capitalizeFirstLetters())
                         .collect(Collectors.joining(" "));
+    }
+
+    private static Function<String, String> capitalizeFirstLetters() {
+        return str -> str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 }
